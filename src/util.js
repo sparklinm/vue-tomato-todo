9d6c4c9afcc2f61c5cca8adaf45da4e3568f7258
fireli96 <343998644@qq.com>
2019-11-22 17:59:56 +0800

    进行待办

--- src/util.js
+++ src/util.js
@@ -1,16 +1,23 @@
 module.exports = {
   timeFormat: function (date, config) {
     const y = date.getFullYear()
-    let m = date.getMonth() + 1
+    const m = date.getMonth() + 1
     const d = date.getDate()
     let time = ''
-    m = m < 10 ? '0' + m : m
     if (config.cut) {
-      time = y + config.cut + m + config.cut + d
+      time = y + config.cut + this.addZero(m, 10) + config.cut + d
     }
     if (config.unit) {
       time = y + '年' + m + '月' + d + '日'
     }
     return time
+  },
+  getTimeView (totalSeconds) {
+    const minutes = Math.floor(totalSeconds / 60)
+    const seconds = totalSeconds - minutes * 60
+    return `${this.addZero(minutes, 10)}:${this.addZero(seconds, 10)}`
+  },
+  addZero (num, limit) {
+    return num < limit ? '0' + num : num
   }
 }
